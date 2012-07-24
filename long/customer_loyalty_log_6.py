93500a8bafeb8b1fb41894d11fb7a99dfd72e2bb
nagofun <cb460770063@126.com>
2012-07-24 17:49:46 +0800

    liucun

---
+++ long/customer_loyalty_log_6.py
@@ -0,0 +1,61 @@
+import MySQLdb
+import sys
+import os
+import time
+import datetime
+
+conn = MySQLdb.connect(host='192.168.0.254',user='root',passwd='123456', db='sacd')
+cursor = conn.cursor()
+year = time.strftime('%Y')
+month = time.strftime('%m')
+day = time.strftime('%d')
+
+today = time.strftime('%Y-%m-%d')
+yesterday = time.strftime('%Y-%m-%d',[int(year),int(month),int(day)-1,0,0,0,0,0,0])
+
+#yesterday 00:00:00 to today 00:00:00
+starttime = time.mktime([int(year),int(month),int(day)-1,0,0,0,0,0,0])
+endtime = time.mktime([int(year),int(month),int(day),0,0,0,0,0,0])
+
+#get records already exist in database
+cursor.execute("select * from customer_loyalty_1",[])
+oldrecords=cursor.fetchall ()
+databaserecords={i[1]:[i[2],i[3],i[4]] for i in oldrecords}
+
+#get register data of yesterday
+cursor.execute("select * from tag_1 where `action` = 'new_user' and `time` between  %s and %s",[starttime, endtime])
+newrecords=cursor.fetchall ()
+sqldata=[[rec[1],yesterday,yesterday,0] for rec in newrecords]
+
+for i in sqldata:
+    #no repreat
+    if i[0] in databaserecords:
+        continue;    
+    cursor.execute("insert into customer_loyalty_1 (`user_id`,`register_date`,`latest_login_date`,`loyal_dates`) values (%s,%s,%s,%s)",i)
+    
+cursor.close ()
+conn.commit()
+
+'''
+update 'latest_login_date' and 'loyal_dates'
+'''
+#get login data of yesterday
+cursor = conn.cursor()
+cursor.execute("select * from tag_1 where `action` = 'login' and `time` between  %s and %s",[starttime, endtime])
+loginrecords=cursor.fetchall ()
+newloginrecords={i[1]:i[5] for i in loginrecords}
+
+updaterecords={}
+for i in newloginrecords:
+    if i in databaserecords:
+        t = time.localtime(newloginrecords[i])#time.strptime(newloginrecords[i], "%Y-%m-%d")
+        y,m,d = t[0:3]
+        updaterecords[i]=[datetime.date(y,m,d),(datetime.date(y,m,d)-databaserecords[i][0]).days]
+        #to update latest_login_date and loyal_dates of YesterdayLOGIN
+        #and neglect users registered before 2012-07-23
+        cursor.execute("update customer_loyalty_1 set `latest_login_date` = %s , `loyal_dates` = %s where `user_id` = %s",[updaterecords[i][0], updaterecords[i][1],i])
+
+
+cursor.close ()
+conn.commit()
+conn.close()
